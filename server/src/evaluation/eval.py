d7e18ffe400b0c2bd8d907b0d0372dd05d8978a2
Artyom Peshkov <artemyipeshkov@gmail.com>
2024-09-22 01:03:13 +0300

    done

--- server/src/evaluation/eval.py
+++ server/src/evaluation/eval.py
@@ -2,12 +2,12 @@ import regex as re
 import math
 
 # TODO: add "sin", "cos", "tan", "e", "pi"
-_correct_elements_regexp = "([\* \- \+ \/ \d \s \. \^ \( \) \|]*(log)*(sqrt)*)*"
+_correct_elements_regexp = "([\× \- \+ \÷ \d \s \. \^ \( \)]*(log)*(sqrt)*)*"
 
-_operation_replacements = [("log", "math.log"), ("sqrt", "math.sqrt"), ("^", "**")]
+_operation_replacements = [("log", "math.log"), ("sqrt", "math.sqrt"), ("^", "**"), ("×", "*"), ("÷", "/") ]
 
 _invalid_string = "inval"
-_invalid_patterns = ["\+\s*\+", "-\s*-", "\*\s*\*", "/\s*/"]
+_invalid_patterns = ["\+\s*\+", "-\s*-", "\×\s*\×", "÷\s*÷"]
 
 def _check_arithmetic_expression(expr: str) -> bool:
 	return re.fullmatch(_correct_elements_regexp, expr) != None
