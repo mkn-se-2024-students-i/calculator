a21eba6b3aa2e5178c1c0351a668db3dc85e3b00
Artyom <47276660+ArtyomPeshkov@users.noreply.github.com>
2024-09-24 23:34:10 +0300

    fix precision 2 (#34)

--- server/src/evaluation/eval.py
+++ server/src/evaluation/eval.py
@@ -4,7 +4,7 @@ import math
 # TODO: add "sin", "cos", "tan", "e", "pi"
 _correct_elements_regexp = "([\× \- \+ \÷ \d \s \. \^ \( \)]*(log)*(sqrt)*(e)*)*"
 
-_operation_replacements = [("log", "math.log"), ("sqrt", "math.sqrt"), ("^", "**"), ("×", "*"), ("÷", "/") ]
+_operation_replacements = [("log", "math.log10"), ("sqrt", "math.sqrt"), ("^", "**"), ("×", "*"), ("÷", "/") ]
 
 _invalid_string = "inval"
 _invalid_patterns = ["\+\s*\+", "-\s*-", "\×\s*\×", "÷\s*÷"]
@@ -31,8 +31,7 @@ def evaluate_expression(expr: str) -> tuple[str, bool]:
 		if (not _check_arithmetic_expression(expr)):
 			return ("The given expression contains forbidden symbols", False)
 		correct_expr = _change_op_to_python_op(_remove_invalid_sequenses(expr))
-		prec = 1e10
-		res = (int(eval(correct_expr) * prec) / prec).__str__()
+		res = eval(correct_expr).__str__()
 		return (res, True)
 	except:
 		return ("The given expression is not correct arithmetic expression", False)
